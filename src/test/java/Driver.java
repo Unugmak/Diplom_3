ac699dcc704640b6952404e6e6192c5d97005d95
Unugmak <mixail.kazakov.1989@yandex.ru>
2023-10-10 00:26:43 +0300

    добавил выбор браузера + отчет

--- src/test/java/Driver.java
+++ src/test/java/Driver.java
@@ -1,23 +1,24 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebDriver;
-
 
 public class Driver {
     WebDriver driver;
 
     public void webDriver() {
-        //Хром
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        String browserType = System.getProperty("browserType");
 
-        //Яндекс браузера
-//        System.setProperty("webdriver.chrome.driver", "D:\\Java\\Diplom\\yandexdriver.exe");
-//        ChromeOptions options = new ChromeOptions();
-//        options.setBinary("C:\\Users\\Unugmak\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        driver = new ChromeDriver(options);
+        if (browserType == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+        } else if (browserType.equalsIgnoreCase("yandex")) {
+            System.setProperty("webdriver.chrome.driver", "D:\\Java\\Diplom\\yandexdriver\\yandexdriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("C:\\Users\\Unugmak\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+        }
     }
 }
